df3297b6805c1f77faabcf268672eca9edbb4b61
edoh <edoh.dev@gmail.com>
2018-03-26 00:23:06 +0200

    improve code formatting

--- js/app.js
+++ js/app.js
@@ -26,19 +26,14 @@ function restart() {
 	document.querySelector('.wrap').classList.add('wrap-hide');
 }
 
-document.querySelector('.restart').addEventListener('click', restart)
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+document.querySelector('.restart').addEventListener('click', restart);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function changeCardOrder() {
 	cards = shuffle(cards);
 	const deck = document.querySelector('.deck');
 	const card = document.querySelector('.card');
+	
 	while (deck.firstChild) {
 	    deck.removeChild(deck.firstChild);
 	}
@@ -122,13 +117,4 @@ function displayStars(move) {
 		star.classList.add('fa-star-o');
 	}
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
